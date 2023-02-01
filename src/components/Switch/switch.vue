7bb1c95ecb23a41b63c4939a99a46d0930252df2
HeroTch <herotch254@gmail.com>
2023-02-01 23:37:25 +0800

    feat(emui-Switch): 添加并注册 Switch 组件

---
+++ src/components/Switch/switch.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'emui-Switch',
+    props: {
+        
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import emuiButton from "./components/Button/button"
+import emuiSwitch from "./components/Switch/switch"
 
 
 Vue.config.productionTip = false
 Vue.component(emuiButton.name,emuiButton)
+Vue.component(emuiSwitch.name,emuiSwitch)
 
 new Vue({
   render: h => h(App),
