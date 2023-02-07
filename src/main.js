5c16816196b25d3ff476f32bbd3b38810df8268e
小娟 戴 <g.daixj@ffcs.cn>
2023-02-08 04:23:20 +0800

    Revert "基础框架，还未完成"
    This reverts commit 65fbddbf0bca92d30d5e8fcf7c6d2660278e7cbf.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-// import Vue from 'vue'
-import Vue from 'vue/dist/vue.esm.js'
+import Vue from 'vue'
 import App from './App.vue'
 import emuiButton from "./components/Button/button"
-import Message from "./components/message/index"
 
 
 Vue.config.productionTip = false
 Vue.component(emuiButton.name,emuiButton)
-Vue.use(Message)
 
 new Vue({
   render: h => h(App),
